d5c44be1b12773c499ca37cac117b5f58d3b1a69
Sam Cook <scook@luc.id>
2019-04-24 12:31:03 +0100

    add script for easier syncing of this repo

---
+++ sync-home.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+sync_files=(".vimrc" ".zshrc" ".zshenv" ".aliases")
+
+if [ -z $1 ]; then
+  for f in ${sync_files[*]};
+  do
+    echo "diffing ${HOME}/${f} ${PWD}/${f}"
+    diff "${HOME}/${f}" "${PWD}/${f}"
+  done
+elif [[ $1 == "--from-home" ]]; then
+  echo "Copying files from ${HOME} to here"
+  for f in ${sync_files[*]};
+  do
+    echo "copying ${f}"
+    cp "${HOME}/${f}" "${PWD}/${f}"
+  done
+elif [[ $1 == "--to-home" ]]; then
+  echo "Copying files from here to ${HOME}"
+  echo "WARNING this will overwrite the versions of these files in ${HOME}"
+  for f in ${sync_files[*]};
+  do
+    echo "copying ${f}"
+    cp "${HOME}/${f}" "${PWD}/${f}"
+  done
+else
+  echo -e "Usage: $0 [OPTIONS]\n"
+  echo -e "diffs SYNC_FILES between ${HOME} and ${PWD}\n"
+  echo -e "OPTIONS"
+  echo -e "--to-home\tcopy SYNC_FILES from ${PWD} to ${HOME}"
+  echo -e "--from-home\tcopy SYNC_FILES from ${HOME} to ${PWD}"
+  echo -e "--help\t\tprint this message"
+  echo -e "\nSYNC_FILES: ${sync_files[*]}"
+fi
