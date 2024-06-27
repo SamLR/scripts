1a41e02730f5e60ca86d06ef74c499571a75a18a
Sam <sam.cook@cint.com>
2024-06-27 10:58:53 +0100

    add custom zsh theme

--- gentoo-custom.zsh-theme
+++ gentoo-custom.zsh-theme
@@ -2,11 +2,8 @@ function prompt_char {
     if [ $UID -eq 0 ]; then echo "#"; else echo $; fi
 }
 
-function current_ruby() {
-    rbenv version-name | sed s/system/`ruby --version | cut -f 2 -d ' '`\*/
-}
+PROMPT='%(!.%{$fg_bold[red]%}.%{$fg[yellow]%}%n@)%m %T %{$fg_bold[magenta]%}%(5~|%-2~/.../%1~|%4~) ($(git_super_status)%{$fg_bold[magenta]%}) [$(kubectx_prompt_info)] {${AWS_REGION:-us-east-1}} %_$(prompt_char)%{$reset_color%} '
 
-PROMPT='%(!.%{$fg_bold[red]%}.%{$fg[yellow]%}%n@)%m %T %{$fg_bold[magenta]%}%(5~|%-2~/.../%1~|%4~) $(tf_prompt_info) ($(git_prompt_info))%_$(prompt_char)%{$reset_color%} '
-
+RPROMPT=''
 ZSH_THEME_GIT_PROMPT_PREFIX=""
 ZSH_THEME_GIT_PROMPT_SUFFIX=""
--- sync-home.sh
+++ sync-home.sh
@@ -14,6 +14,8 @@ if [ -z $1 ]; then
     echo "diffing ${HOME}/${f} ${PWD}/${f}"
     $diff_cmd "${HOME}/${f}" "${PWD}/${f}"
   done
+
+  $diff_cmd "${HOME}/.oh-my-zsh/custom/themes/gentoo-custom.zsh-theme" "${PWD}/gentoo-custom.zsh-theme"
 elif [[ $1 == "--from-home" ]]; then
   echo "Copying files from ${HOME} to here"
   for f in ${sync_files[*]};
@@ -21,6 +23,7 @@ elif [[ $1 == "--from-home" ]]; then
     echo "copying ${f} to here"
     cp "${HOME}/${f}" "${PWD}/${f}"
   done
+  cp "${HOME}/.oh-my-zsh/custom/themes/gentoo-custom.zsh-theme" "${PWD}/gentoo-custom.zsh-theme"
 elif [[ $1 == "--to-home" ]]; then
   echo "Copying files from here to ${HOME}"
   echo "WARNING this will overwrite the versions of these files in ${HOME}"
@@ -29,6 +32,7 @@ elif [[ $1 == "--to-home" ]]; then
     echo "copying ${f} to home"
     cp "${PWD}/${f}" "${HOME}/${f}"
   done
+  cp "${PWD}/gentoo-custom.zsh-theme" "${HOME}/.oh-my-zsh/custom/themes/gentoo-custom.zsh-theme"
 else
   echo -e "Usage: $0 [OPTIONS]\n"
   echo -e "diffs SYNC_FILES between ${HOME} and ${PWD}\n"
@@ -36,5 +40,5 @@ else
   echo -e "--to-home\tcopy SYNC_FILES from ${PWD} to ${HOME}"
   echo -e "--from-home\tcopy SYNC_FILES from ${HOME} to ${PWD}"
   echo -e "--help\t\tprint this message"
-  echo -e "\nSYNC_FILES: ${sync_files[*]}"
+  echo -e "\nSYNC_FILES: ${sync_files[*]} gentoo-custom.zsh-theme"
 fi
